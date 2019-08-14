62ead9a25f818444db3f63504f8f3c44333a8d3f
Edward Sugitpibul <etsugitpibul@gmail.com>
2019-08-14 20:55:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var recipes = {}
 
+<<<<<<< HEAD
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value});
 }
@@ -19,6 +20,10 @@ function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key]
   return object
 }
+=======
+
+
+>>>>>>> 5fdc037b61622c3021a30ab8eb1db5c15408183c
 
 
 
